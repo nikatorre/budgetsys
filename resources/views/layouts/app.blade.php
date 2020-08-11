cfa35980f9f44b31216388f1aa8fe49d27608544
nikatorre <torrezenica@gmail.com>
2020-08-11 12:20:03 +0800

    template

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,23 +1,55 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>Login Register | Notika - Notika Admin Template</title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- favicon
+  ============================================ -->
+  <link rel="shortcut icon" type="image/x-icon" href="{{asset('img/favicon.ico')}}">
+  <!-- Google Fonts
+  ============================================ -->
+  <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,700,900" rel="stylesheet">
+  <!-- Bootstrap CSS
+  ============================================ -->
+  <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+  <!-- font awesome CSS
+  ============================================ -->
+  <link rel="stylesheet" href="{{asset('css/font-awesome.min.css')}}">
+  <!-- owl.carousel CSS
+  ============================================ -->
+  <link rel="stylesheet" href="{{asset('css/owl.carousel.css')}}">
+  <link rel="stylesheet" href="{{asset('css/owl.theme.css')}}">
+  <link rel="stylesheet" href="{{asset('css/owl.transitions.css')}}">
+  <!-- animate CSS
+  ============================================ -->
+  <link rel="stylesheet" href="{{asset('css/animate.css')}}">
+  <!-- normalize CSS
+  ============================================ -->
+  <link rel="stylesheet" href="{{asset('css/normalize.css')}}">
+  <!-- mCustomScrollbar CSS
+  ============================================ -->
+  <link rel="stylesheet" href="{{asset('css/scrollbar/jquery.mCustomScrollbar.min.css')}}">
+  <!-- wave CSS
+  ============================================ -->
+  <link rel="stylesheet" href="{{asset('css/wave/waves.min.css')}}">
+  <!-- Notika icon CSS
+  ============================================ -->
+  <link rel="stylesheet" href="{{asset('css/notika-custom-icon.css')}}">
+  <!-- main CSS
+  ============================================ -->
+  <link rel="stylesheet" href="{{asset('css/main.css')}}">
+  <!-- style CSS
+  ============================================ -->
+  <link rel="stylesheet" href="{{asset('style.css')}}">
+  <!-- responsive CSS
+  ============================================ -->
+  <link rel="stylesheet" href="{{asset('css/responsive.css')}}">
+  <!-- modernizr JS
+  ============================================ -->
+  <script src="{{asset('js/vendor/modernizr-2.8.3.min.js')}}"></script>
 </head>
 <body>
     <div id="app">
@@ -76,5 +108,71 @@
             @yield('content')
         </main>
     </div>
+    <!-- jquery
+		============================================ -->
+    <script src="{{asset('')}}"></script>
+    <!-- bootstrap JS
+		============================================ -->
+    <script src="{{asset('js/bootstrap.min.js')}}"></script>
+    <!-- wow JS
+		============================================ -->
+    <script src="{{asset('js/wow.min.js')}}"></script>
+    <!-- price-slider JS
+		============================================ -->
+    <script src="{{asset('js/jquery-price-slider.js')}}"></script>
+    <!-- owl.carousel JS
+		============================================ -->
+    <script src="{{asset('js/owl.carousel.min.js')}}"></script>
+    <!-- scrollUp JS
+		============================================ -->
+    <script src="{{asset('js/jquery.scrollUp.min.js')}}"></script>
+    <!-- meanmenu JS
+		============================================ -->
+    <script src="{{asset('js/meanmenu/jquery.meanmenu.js')}}"></script>
+    <!-- counterup JS
+		============================================ -->
+    <script src="{{asset('js/counterup/jquery.counterup.min.js')}}"></script>
+    <script src="{{asset('js/counterup/waypoints.min.js')}}"></script>
+    <script src="{{asset('js/counterup/counterup-active.js')}}"></script>
+    <!-- mCustomScrollbar JS
+		============================================ -->
+    <script src="{{asset('js/scrollbar/jquery.mCustomScrollbar.concat.min.js')}}"></script>
+    <!-- sparkline JS
+		============================================ -->
+    <script src="{{asset('js/sparkline/jquery.sparkline.min.js')}}"></script>
+    <script src="{{asset('js/sparkline/sparkline-active.js')}}"></script>
+    <!-- flot JS
+		============================================ -->
+    <script src="{{asset('js/flot/jquery.flot.js')}}"></script>
+    <script src="{{asset('js/flot/jquery.flot.resize.js')}}"></script>
+    <script src="{{asset('js/flot/flot-active.js')}}"></script>
+    <!-- knob JS
+		============================================ -->
+    <script src="{{asset('js/knob/jquery.knob.js')}}"></script>
+    <script src="{{asset('js/knob/jquery.appear.js')}}"></script>
+    <script src="{{asset('js/knob/knob-active.js')}}"></script>
+    <!--  wave JS
+		============================================ -->
+    <script src="{{asset('js/wave/waves.min.js')}}"></script>
+    <script src="{{asset('js/wave/wave-active.js')}}"></script>
+    <!-- icheck JS
+		============================================ -->
+    <script src="{{asset('js/icheck/icheck.min.js')}}"></script>
+    <script src="{{asset('js/icheck/icheck-active.js')}}"></script>
+    <!--  Chat JS
+		============================================ -->
+    <script src="{{asset('js/chat/jquery.chat.js')}}"></script>
+    <!--  todo JS
+		============================================ -->
+    <script src="{{asset('js/todo/jquery.todo.js')}}"></script>
+    <!-- plugins JS
+		============================================ -->
+    <script src="{{asset('js/plugins.js')}}"></script>
+    <!-- main JS
+		============================================ -->
+    <script src="{{asset('js/main.js')}}"></script>
+	<!-- tawk chat JS
+		============================================ -->
+    <script src="{{asset('js/tawk-chat.js')}}"></script>
 </body>
 </html>
