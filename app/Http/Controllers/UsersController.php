f7747307086e95d94ef21e2f3276b8ef00ded8ed
Denver Arancillo <denver.arancillo@gmail.com>
2020-08-18 21:55:33 +0800

    fix register page

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -6,9 +6,9 @@ use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
-    //
-    public function index()
-    {
-      echo "code will be here";
-    }
+	//
+	public function index()
+	{
+		echo "code will be here";
+	}
 }
